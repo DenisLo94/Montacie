cd688643a884d33ae4471bf6b3d14882309613c0
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2018-10-18 09:45:13 +0200

    Squelette du 1er exercice

---
+++ Montacié Programmation Objet et Groupware/src/atelier04/BranchementConditionel1.java
@@ -0,0 +1,16 @@
+package atelier04;
+
+public class BranchementConditionel1 {
+
+	public static void main(String[] args) {
+		
+			int p1 = 10, p2 = 9, p3 = -1, p4;
+			
+			
+			int p4;
+		
+			System.out.println(p4) ;
+		
+	}
+
+}
