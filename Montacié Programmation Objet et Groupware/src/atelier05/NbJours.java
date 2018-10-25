9f88f45fa42554e8e69cb075e31460c7b14a9694
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2018-10-25 10:37:10 +0200

    Atelier 5 correction exercice 1

--- Montacié Programmation Objet et Groupware/src/atelier05/NbJours.java
+++ Montacié Programmation Objet et Groupware/src/atelier05/NbJours.java
@@ -6,6 +6,16 @@ public class NbJours {
 
 	public static void main(String[] args) {
 		Date fete = new Date(14,7,2019);
+		// on entre la date du jour au clavier
+		Date aujour = new Date();
+		// on calcule le nombre de jours entre les deux dates
+		int nbJours=0;
+		while (fete.CompareTo(aujour) == false) {
+			nbJours++;
+			aujour.Incrementer();
+		}
+		System.out.println("le nombre de jours est " + nbJours);
+			
 	}
 
 }
