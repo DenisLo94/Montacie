fdd51ad8146a6c438a8c43f73c5844c58233c5d1
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2018-12-13 09:47:38 +0100

    Squelette de LireToutUnTexte

---
+++ Montacié Programmation Objet et Groupware/src/atelier11/LireToutUnTexte.java
@@ -0,0 +1,21 @@
+package atelier11;
+public class LireToutUnTexte {
+private String Stexte;
+/** lecture d'un fichier texte dans la chaîne sTexte
+* @param ft descripteur du fichier
+*/
+public LireToutUnTexte(File ft) {
+}
+/** Recherche du nombre d'occurences de s dans sTexte
+* @param s chaîne cherchée
+* @return nombre d'occurences
+*/
+public int Chercher(String s) {
+}
+
+/** Affichage du contexte des occurences de s dans sTexte
+* @param s chaîne cherchée
+* @param taille taille du contexte
+*/
+public void ChercherVoir(String s, int taille) {
+}
