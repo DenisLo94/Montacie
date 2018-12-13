74aea0415fcc6dc7253dff41557eb33fc364aa68
montacie <isha@d219-tyratron.paris-sorbonne.fr>
2018-12-13 10:23:24 +0100

    Corrigé du constructeur de LireToutUnTexe

--- Montacié Programmation Objet et Groupware/src/atelier11/LireToutUnTexte.java
+++ Montacié Programmation Objet et Groupware/src/atelier11/LireToutUnTexte.java
@@ -1,21 +1,43 @@
 package atelier11;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+
 public class LireToutUnTexte {
 private String Stexte;
 /** lecture d'un fichier texte dans la chaîne sTexte
 * @param ft descripteur du fichier
 */
 public LireToutUnTexte(File ft) {
+	String ligne;
+	Stexte = "";
+	try {
+		BufferedReader br = new BufferedReader(new FileReader(ft));
+		while ((ligne=br.readLine()) != null) {
+			// ligne = ligne.split("\t")[0];
+			Stexte += ligne + " ";
+		}
+		br.close();
+	} 
+	catch (IOException e) { e.printStackTrace(); }
 }
+
 /** Recherche du nombre d'occurences de s dans sTexte
 * @param s chaîne cherchée
 * @return nombre d'occurences
 */
 public int Chercher(String s) {
+	return 0;
 }
+
 
 /** Affichage du contexte des occurences de s dans sTexte
 * @param s chaîne cherchée
 * @param taille taille du contexte
 */
 public void ChercherVoir(String s, int taille) {
+	return;
+}
 }
