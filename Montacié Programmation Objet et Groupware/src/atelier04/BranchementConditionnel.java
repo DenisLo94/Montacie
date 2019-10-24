f5ea3970dcd2d58285cce8643f92921a28801329
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2019-10-24 10:44:47 +0200

    Correction Branchement conditionnel minimum

--- Montacié Programmation Objet et Groupware/src/atelier04/BranchementConditionnel.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/BranchementConditionnel.java
@@ -4,6 +4,13 @@ public class BranchementConditionnel {
 
 	public static void main(String[] args) {
 		int p1 = 10, p2 = 9, p3 = -1;
+
+		System.out.println(maximum(p1, p2, p3));
+		System.out.println(minimum(p1, p2, p3));
+
+	}
+
+	public static int maximum(int p1, int p2, int p3) {
 		int p4;
 
 		if (p1 < p2) {
@@ -16,7 +23,22 @@ public class BranchementConditionnel {
 		else
 			p4 = p1;
 
-		System.out.println(p4);
+		return p4;
+	}
 
+	public static int minimum(int p1, int p2, int p3) {
+		int p4;
+
+		if (p1 < p2) {
+			if (p1 < p3)
+				p4 = p1;
+			else
+				p4 = p3;
+		} else if (p2 < p3)
+			p4 = p2;
+		else
+			p4 = p3;
+
+		return p4;
 	}
 }
