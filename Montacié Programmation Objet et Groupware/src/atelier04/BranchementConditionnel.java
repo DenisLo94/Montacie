14c34b7a94ed41219be42750270ca00b610236e3
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2019-10-24 11:40:07 +0200

    correction branchement conditionnel avec mediane et test

--- Montacié Programmation Objet et Groupware/src/atelier04/BranchementConditionnel.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/BranchementConditionnel.java
@@ -5,8 +5,35 @@ public class BranchementConditionnel {
 	public static void main(String[] args) {
 		int p1 = 10, p2 = 9, p3 = -1;
 
+		p1 = 10; p2 = 9; p3 = -1;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
 		System.out.println(maximum(p1, p2, p3));
-		System.out.println(minimum(p1, p2, p3));
+
+		p1 = 9; p2 = 10; p3 = -1;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
+		System.out.println(maximum(p1, p2, p3));
+
+		p1 = -1; p2 = 9; p3 = 10;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
+		System.out.println(maximum(p1, p2, p3));
+
+		p1 = 9; p2 = -1; p3 = 10;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
+		System.out.println(maximum(p1, p2, p3));
+
+		p1 = 10; p2 = -1; p3 = 9;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
+		System.out.println(maximum(p1, p2, p3));
+
+		p1 = -1; p2 = 10; p3 = 9;
+		System.out.print(minimum(p1, p2, p3) + " ");
+		System.out.print(mediane(p1, p2, p3) + " ");
+		System.out.println(maximum(p1, p2, p3));
 
 	}
 
@@ -38,6 +65,31 @@ public class BranchementConditionnel {
 			p4 = p2;
 		else
 			p4 = p3;
+
+		return p4;
+	}
+
+	public static int mediane(int p1, int p2, int p3) {
+		int p4;
+
+		if (p1 < p2) {
+			if (p2 < p3)
+				p4 = p2;
+			else {
+				if (p1 > p3)
+					p4 = p3;
+				else
+					p4 = p1;
+			}
+
+		} else if (p1 < p3)
+			p4 = p1;
+		else {
+			if (p3 > p2)
+				p4 = p2;
+			else
+				p4 = p3;
+		}
 
 		return p4;
 	}
