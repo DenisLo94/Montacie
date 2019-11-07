16f3801dac51b4e2cbba11d2321e14a879a3c389
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2019-11-07 10:38:55 +0100

    nbJoursDepuis

--- Montacié Programmation Objet et Groupware/src/atelier05/nbJoursDepuis.java
+++ Montacié Programmation Objet et Groupware/src/atelier05/nbJoursDepuis.java
@@ -1,9 +1,23 @@
 package atelier05;
 
+import cours04.Date;
+
 public class nbJoursDepuis {
 
 	public static void main(String[] args) {
-		Date aujourdhui = new Date(7,11,2019);
+		Date aujourdhui = new Date(7, 11, 2019);
+
+		int nbJours = 0;
+		Date dt = new Date();
+		System.out.print("le nombre de jours depuis ");
+		dt.Afficher();
+		do {
+			nbJours++;
+		    dt.Incrementer();
+		}
+		while (dt.CompareTo(aujourdhui));
+
+		System.out.println("= " + nbJours + " jours");
 
 	}
 
